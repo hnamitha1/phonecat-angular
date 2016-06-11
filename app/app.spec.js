e80de3df2ca2a3b3cd27fd4665f01432480f457d
hnamitha1 <namitha.h@gmail.com>
2016-06-11 18:48:49 +0100

    components

--- app/app.spec.js
+++ app/app.spec.js
@@ -1,17 +1,15 @@
-describe('PhoneListController', function() {
+describe('phoneList', function() {
 
   beforeEach(module('phonecatApp'));
 
-  it('should create a `phones` model with 3 phones', inject(function($controller) {
-    var scope = {};
-    var ctrl = $controller('PhoneListController', {$scope: scope});
+  describe('PhoneListController', function() {
 
-    expect(scope.phones.length).toBe(3);
-  }));
+    it('should create a `phones` model with 3 phones', inject(function($componentController) {
+      var ctrl = $componentController('phoneList');
 
-  it('should create a `phones` model with 3 phones', inject(function($controller) {
-    var scope = {};
-    var ctrl = $controller('PhoneListController', {$scope: scope});
-    expect(scope.name).toBe('world');
-  }));  
+      expect(ctrl.phones.length).toBe(3);
+    }));
+
+  });
+
 });
