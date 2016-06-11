e837d7f207da2f1afc898ffe5884d792c6d11d42
hnamitha1 <namitha.h@gmail.com>
2016-06-11 18:19:00 +0100

    Angular Templates

---
+++ app/app.spec.js
@@ -0,0 +1,17 @@
+describe('PhoneListController', function() {
+
+  beforeEach(module('phonecatApp'));
+
+  it('should create a `phones` model with 3 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+
+    expect(scope.phones.length).toBe(3);
+  }));
+
+  it('should create a `phones` model with 3 phones', inject(function($controller) {
+    var scope = {};
+    var ctrl = $controller('PhoneListController', {$scope: scope});
+    expect(scope.name).toBe('world');
+  }));  
+});
