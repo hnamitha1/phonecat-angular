6638734ee24255514a831112193b4a1f72051053
hnamitha1 <namitha.h@gmail.com>
2016-06-11 20:54:36 +0100

    dependency injection

--- app/phone-list/phone-list.component.js
+++ app/phone-list/phone-list.component.js
@@ -1,22 +1,15 @@
-  angular.
+angular.
   module('phoneList').
-  component('phoneList', {templateUrl: 'phone-list/phone-list.template.html',
-     controller: function PhoneListController() {
-       this.phones = [
-         {
-           name: 'Nexus S',
-           snippet: 'Fast just got faster with Nexus S.',
-           age: 1
-         }, {
-           name: 'Motorola XOOM™ with Wi-Fi',
-           snippet: 'The Next, Next Generation tablet.',
-           age: 2
-         }, {
-           name: 'MOTOROLA XOOM™',
-           snippet: 'The Next, Next Generation tablet.',
-           age: 3
-         }
-       ];
-       this.orderProp = 'age';
-     }
-   });
+  component('phoneList', {
+    templateUrl: 'phone-list/phone-list.template.html',
+    controller: ['$http',
+      function PhoneListController($http) {
+        var self = this;
+        self.orderProp = 'age';
+
+        $http.get('phones/phones.json').then(function(response) {
+          self.phones = response.data;
+        });
+      }
+    ]
+  });
